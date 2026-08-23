d589d8d50dbb6971e5f5bd25e2b8b8b8e0c0185d
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-23 20:08:58 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as ClaudiaReactions } from './ClaudiaReactions';
+export type { ClaudiaReactionsProps } from './ClaudiaReactions';
+export type { ClaudiaReaction, ClaudiaReactionKind } from './types';
